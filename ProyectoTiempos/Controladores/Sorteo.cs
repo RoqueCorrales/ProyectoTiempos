f05e4cb9fb54e3fe1c1d860d97d3d9d3d460e7fc
Roque Chacon Corrales <roke12chacon@gmail.com>
2017-03-11 17:37:18 -0600

    Correctov1

---
+++ ProyectoTiempos/Controladores/Sorteo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBAccess;
+using System.Data;
+
+namespace ProyectoTiempos.Controladores
+{
+   public  class Sorteo : ErrorHandler
+    {
+        private Modelo.Sorteo sorteo;
+        public Sorteo()
+        {
+            this.sorteo = new Modelo.Sorteo();
+        }
+        public void Insert(string descripcion, string codigo, DateTime fecha,  Boolean estado)
+        {
+            this.sorteo = new Modelo.Sorteo(descripcion, codigo, fecha, estado);
+            this.sorteo.Insert();
+            if (this.sorteo.isError)
+            {
+                this.isError = true;
+                this.errorDescription = this.sorteo.errorDescription;
+            }
+        }
+
+        public DataTable Select(string codigo)
+        {
+            DataTable result = new DataTable();
+            result = this.sorteo.Select(codigo);
+            if (this.sorteo.isError)
+            {
+                this.isError = true;
+                this.errorDescription = this.sorteo.errorDescription;
+            }
+            return result;
+        }
+
+
+
+    }
+}
+
---
+++ ProyectoTiempos/Modelo/Sorteo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBAccess;
+using System.Data;
+
+namespace ProyectoTiempos.Modelo
+{
+    public class Sorteo : ErrorHandler
+    {
+        public int id { set; get; }
+        public DateTime fecha { set; get; }
+        public string descripcion { set; get; }
+        public Boolean estado { set; get; }
+        public string codigo { set; get; }
+
+        public Sorteo()
+        {
+                
+        }
+     
+
+        public Sorteo(string descripcion, string codigo, DateTime fecha, bool estado)
+        {
+            this.descripcion = descripcion;
+            this.codigo = codigo;
+            this.fecha = fecha;
+            this.estado = estado;
+        }
+
+        public DataTable Select()
+        {
+            DataTable result = Program.da.SqlQuery("select * from sorteo;", new Dictionary<string, object>());
+            if (Program.da.isError)
+            {
+                this.isError = true;
+                this.errorDescription = Program.da.errorDescription;
+            }
+            return result;
+        }
+
+        public void Insert()
+        {
+            Dictionary<string, object> parametros = new Dictionary<string, object>();
+            parametros.Add("fecha", this.fecha);
+            parametros.Add("estado", this.estado);
+            parametros.Add("descripcion", this.descripcion);
+            parametros.Add("codigo", this.codigo);
+            
+            DataTable result = Program.da.SqlQuery("insert into sorteo(descripcion,fecha,estado,codigo ) values (@fecha ,@estado,@descripcion,@codigo) returning id;", parametros);
+            if (Program.da.isError)
+            {
+                this.isError = true;
+                this.errorDescription = Program.da.errorDescription;
+                return;
+            }
+            if (result.Rows.Count > 0)
+            {
+                this.id = Convert.ToInt32(result.Rows[0]["id"]);
+            }
+
+
+
+
+        }
+        public DataTable Select(string codigo)
+        {
+
+            DataTable result = Program.da.SqlQuery("select * from sorteo where codigo = '" + codigo + "'", new Dictionary<string, object>());
+            if (Program.da.isError)
+            {
+                this.isError = true;
+                this.errorDescription = Program.da.errorDescription;
+            }
+            return result;
+        }
+
+
+    }
+}
