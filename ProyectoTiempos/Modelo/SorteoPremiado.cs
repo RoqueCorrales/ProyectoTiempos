71b57504eba5d04440e2af7473c21c332c64ebc8
Roque Chacon Corrales <roke12chacon@gmail.com>
2017-03-17 19:04:01 -0600

    AgregandoPago al sorteo

--- ProyectoTiempos/Modelo/SorteoPremiado.cs
+++ ProyectoTiempos/Modelo/SorteoPremiado.cs
@@ -16,6 +16,7 @@ namespace ProyectoTiempos.Modelo
         private int numDos { set; get; }
         private int numTres { set; get; }
         private int id_sorteo { set; get; }
+        private Boolean pagado { set; get; }
 
 
 
@@ -23,14 +24,16 @@ namespace ProyectoTiempos.Modelo
 
         public void Insert( string codigo_sorteo,int numUno,int numDos,int numTres, int id_sorteo)
         {
+            Boolean pagado = false;
             Dictionary<string, object> parametros = new Dictionary<string, object>();
             parametros.Add("numeroUno", numUno);
             parametros.Add("numeroDos", numDos);
             parametros.Add("numeroTres", numTres);
             parametros.Add("id_sorteo", id_sorteo);
             parametros.Add("codigo_sorteo", codigo_sorteo);
+            parametros.Add("pagado", pagado);
 
-            DataTable result = Program.da.SqlQuery("insert into numPremiados (numerouno, numerodos, numerotres, id_sorteo, codigo_sorteo ) values (@numerouno, @numerodos, @numerotres, @id_sorteo, @codigo_sorteo ) returning id;", parametros);
+            DataTable result = Program.da.SqlQuery("insert into numPremiados (numerouno, numerodos, numerotres, id_sorteo, codigo_sorteo, pagado ) values (@numerouno, @numerodos, @numerotres, @id_sorteo, @codigo_sorteo, @pagado) returning id;", parametros);
             if (Program.da.isError)
             {
                 this.isError = true;
